ed64479ce3960dd54dbf0266502c7ae691784eea
bp9614 <bpham9614@gmail.com>
2017-10-16 17:10:22 -0400

    Comments and exclusion file

--- concordance.py
+++ concordance.py
@@ -1,7 +1,18 @@
 import re
 
 
-def split_file(file: str, encoding: str = None) -> (list, set):
+def parse_file(file: str, encoding: str = None) -> (list, set):
+    """Parses the file for lines, words in the line, and words in the file.
+
+    Reads the file to get every line and numbers them and combines it with a
+    set of every word in that line, then creates a set of every word
+    (lower case) in the file. Any blank line is removed, but they still
+    contribute to the line number.
+    :param file: Sample file to extract lines and words from.
+    :param encoding: Character set used by the file.
+    :return: A list of tuples containing each line and the words in them, and
+    a set of every word in the file.
+    """
     with open(file, 'r', encoding=encoding) as smp_file:
         sample = smp_file.read().splitlines()
 
@@ -14,8 +25,39 @@ def split_file(file: str, encoding: str = None) -> (list, set):
     return lines, dictionary
 
 
-def exclude(dictionary: set, exclude_words: iter = None, file: str = None,
-            encoding: str = None) -> None:
+def exclude(dictionary: set, exclude_words: iter = None,
+            file: str = None, encoding: str = None) -> set:
+    """Returns the set after removing any the given words from it.
+
+    :param dictionary: Set of words to have words removed from.
+    :param exclude_words: Iterable that contains values to remove from
+    dictionary.
+    :param file: File containing words to remove from dictionary.
+    :param encoding: Character set used by the file.
+    :return: Set of words with the given words removed from it.
+    """
+    if not exclude_words:
+        exclude_words = set()
+
+    if file:
+        with open(file, 'r', encoding=encoding) as excluding:
+            exclude_words = ({word for line in excluding.read().splitlines()
+                              for word in re.split("[^a-z']", line.lower())} |
+                             exclude_words)
+
+    return dictionary - exclude_words
+
+
+def exclude_update(dictionary: set, exclude_words: iter = None,
+                   file: str = None, encoding: str = None) -> None:
+    """Removes any instance of the given words from the set.
+
+    :param dictionary: Set of words to have words removed from.
+    :param exclude_words: Iterable that contains values to remove from
+    dictionary.
+    :param file: File containing words to remove from dictionary.
+    :param encoding: Character set used by the file.
+    """
     if not exclude_words:
         exclude_words = set()
 
@@ -28,7 +70,14 @@ def exclude(dictionary: set, exclude_words: iter = None, file: str = None,
     dictionary.difference_update(exclude_words)
 
 
-def get_concordance(lines: list, dictionary: set, output:str = None) -> None:
+def get_concordance(lines: list, dictionary: set, output: str = None) -> None:
+    """Creates the concordance and outputs it to a file or the terminal.
+
+    :param lines: A list of sentences along with each word in that line.
+    :param dictionary: A set of words to generate a concordance from the lines.
+    :param output: Outputs the results to a file if given or the terminal if
+    not.
+    """
     if output:
         output = open(output, 'w')
 
@@ -43,8 +92,8 @@ def get_concordance(lines: list, dictionary: set, output:str = None) -> None:
 
 
 def main():
-    lines, dictionary = split_file('sample/greenEggs.txt')
-    # exclude(dictionary, exclude_words={'a', 'of', 'the'})
+    lines, dictionary = parse_file('sample/greenEggs.txt')
+    # exclude_update(dictionary, file='sample/ExcludeTheseWords.txt')
     get_concordance(lines, dictionary, 'sample/greenEggsOutput.txt')
 
 
